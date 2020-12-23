0f36e7a3b5b7200e6b7f69cb24721b9207e9757d
Augustin Peyrard <augustin.peyrard@gmail.com>
2020-12-23 10:52:33 -0800

    adds day2 part2
    Quite straightforward, maybe next days will be a bit more
    challenging...

--- aoc/day2/match_passwords.py
+++ aoc/day2/match_passwords.py
@@ -20,6 +20,22 @@ but needs at least 1. The first and third passwords are valid: they contain one 
 their respective policies.
 How many passwords are valid according to their policies?
 
+--- Part Two ---
+
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate
+Authentication System is expecting.
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at
+the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second
+character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these
+positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+Given the same example list from above:
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
+
+
 """
 import os
 import re
@@ -48,6 +64,20 @@ def _validate_password(password: PasswordEntry) -> bool:
     return counter >= password["lowest"]
 
 
+def validate_passwords_part2(passwords: List[PasswordEntry]) -> int:
+    return sum(map(_validate_password_part2, passwords))
+
+
+def _validate_password_part2(password: PasswordEntry) -> bool:
+    password_text = password["password"]
+    password_char = password["char"]
+
+    first = password_text[password["lowest"] - 1]
+    second = password_text[password["highest"] - 1]
+
+    return (first == password_char or second == password_char) and first != second
+
+
 RAW_PASSWORD_REGEX = re.compile(r"^(\d+)-(\d+) (\w): (.*)")
 
 
@@ -74,3 +104,6 @@ if __name__ == "__main__":
 
         solution = validate_passwords(input_passwords)
         print(f"solution (part1): {solution}")
+
+        solution_part2 = validate_passwords_part2(input_passwords)
+        print(f"solution (part2): {solution_part2}")
--- tests/day2/test_match_passwords.py
+++ tests/day2/test_match_passwords.py
@@ -1,6 +1,6 @@
 from hamcrest import assert_that, equal_to
 
-from aoc.day2.match_passwords import _parse_raw_password_item, validate_passwords
+from aoc.day2.match_passwords import _parse_raw_password_item, validate_passwords, validate_passwords_part2
 
 
 class TestValidatePasswords:
@@ -21,3 +21,23 @@ class TestValidatePasswords:
 
         # THEN
         assert_that(res, equal_to(2))
+
+
+class TestValidatePasswordsPart2:
+    def test_should_validate_given_example(self):
+        # GIVEN
+        # noinspection SpellCheckingInspection
+        passwords = list(map(
+            _parse_raw_password_item,
+            [
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc"
+            ]
+        ))
+
+        # WHEN
+        res = validate_passwords_part2(passwords)
+
+        # THEN
+        assert_that(res, equal_to(1))
