ab980aa8a10b4d7e75ec23c4b1db70a6e89997e3
Augustin Peyrard <augustin.peyrard@gmail.com>
2020-12-24 23:32:54 -0800

    adds day3 part2

--- aoc/day3/tobogan_trajectory.py
+++ aoc/day3/tobogan_trajectory.py
@@ -55,9 +55,25 @@ In this example, traversing the map using this slope would cause you to encounte
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you
 encounter?
 
+--- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left
+corner and traverse the map all the way to the bottom:
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together,
+these produce the answer 336.
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+
 """
 import os
-from typing import List
+from math import prod
+from typing import List, Tuple
 
 
 def count_trees_in_trajectory(slope: List[List[int]], right: int, down: int) -> int:
@@ -76,6 +92,13 @@ def count_trees_in_trajectory(slope: List[List[int]], right: int, down: int) -> 
     return number_of_trees
 
 
+def analyze_trajectories(slope: List[List[int]], trajectories: List[Tuple[int, int]]) -> int:
+    return prod(map(
+        lambda t: count_trees_in_trajectory(slope, t[0], t[1]),
+        trajectories
+    ))
+
+
 def _parse_line(line: str) -> List[int]:
     return list(map("#".__eq__, line))
 
@@ -88,5 +111,17 @@ if __name__ == "__main__":
             if line
         ]
 
-        solution = count_trees_in_trajectory(input_slope, right=3, down=1)
-        print(f"solution (part1): {solution}")
+        solution_part1 = count_trees_in_trajectory(input_slope, right=3, down=1)
+        print(f"solution (part1): {solution_part1}")
+
+        solution_part2 = analyze_trajectories(
+            input_slope,
+            [
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2)
+            ]
+        )
+        print(f"solution (part2): {solution_part2}")
--- tests/day3/test_tobogan_trajectory.py
+++ tests/day3/test_tobogan_trajectory.py
@@ -1,6 +1,6 @@
 from hamcrest import assert_that, equal_to
 
-from aoc.day3.tobogan_trajectory import _parse_line, count_trees_in_trajectory
+from aoc.day3.tobogan_trajectory import _parse_line, count_trees_in_trajectory, analyze_trajectories
 
 
 class TestCountTreesInTrajectory:
@@ -45,3 +45,64 @@ class TestCountTreesInTrajectory:
 
         # THEN
         assert_that(trees, equal_to(1))
+
+
+class TestAnalyzeTrajectories:
+    def test_should_validate_part1_example(self):
+        # GIVEN
+        slope = list(map(
+            _parse_line,
+            [
+                "..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#"
+            ]
+        ))
+
+        # WHEN
+        trees = analyze_trajectories(slope, [(3, 1)])
+
+        # THEN
+        assert_that(trees, equal_to(7))
+
+    def test_should_validate_part2_example(self):
+        # GIVEN
+        slope = list(map(
+            _parse_line,
+            [
+                "..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#"
+            ]
+        ))
+
+        # WHEN
+        trees = analyze_trajectories(
+            slope,
+            [
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2)
+            ]
+        )
+
+        # THEN
+        assert_that(trees, equal_to(336))
