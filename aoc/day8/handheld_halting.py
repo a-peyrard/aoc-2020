43b26ed63745ea44c04226286969bd2350fe3b8e
Augustin Peyrard <augustin.peyrard@gmail.com>
2020-12-28 22:53:04 -0800

    adds day8 part2
    brute force approach, too late to find a better solution :)
    Anyway this is quadratic, and we don't have that many
    instructions...

--- aoc/day8/handheld_halting.py
+++ aoc/day8/handheld_halting.py
@@ -45,11 +45,46 @@ Immediately before the program would run an instruction a second time, the value
 Run your copy of the boot code. Immediately before any instruction is executed a second time, what value is in the
  accumulator?
 
+--- Part Two ---
+After some careful analysis, you believe that exactly one instruction is corrupted.
+Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp. (No acc instructions
+ were harmed in the corruption of this boot code.)
+The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in
+ the file. By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+For example, consider the same program from above:
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop, never
+ leaving that instruction. If you change almost any of the jmp instructions, the program will still eventually find
+  another jmp instruction and loop forever.
+However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The instructions
+ are visited in this order:
+nop +0  | 1
+acc +1  | 2
+jmp +4  | 3
+acc +3  |
+jmp -3  |
+acc -99 |
+acc +1  | 4
+nop -4  | 5
+acc +6  | 6
+After the last instruction (acc +6), the program terminates by attempting to run the instruction below the last
+ instruction in the file. With this change, after the program terminates, the accumulator contains the value 8
+ (acc +1, acc +1, acc +6).
+Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp). What is the value
+ of the accumulator after the program terminates?
 """
 import os
 import re
 from enum import Enum
-from typing import List, NamedTuple, Iterable
+from typing import List, NamedTuple, Iterable, Tuple, Optional
 
 
 class Type(Enum):
@@ -66,11 +101,11 @@ class Instruction(NamedTuple):
 INSTRUCTION_REGEX = re.compile(r"^(\w+) ([+-]\d+)$")
 
 
-def play_instructions(instructions: List[Instruction]) -> int:
+def play_instructions(instructions: List[Instruction]) -> Tuple[int, bool]:
     executed_instructions = set()
     cursor = 0
     accumulator = 0
-    while cursor not in executed_instructions:
+    while cursor not in executed_instructions and cursor < len(instructions):
         executed_instructions.add(cursor)
         instruction_type, value = instructions[cursor]
         if instruction_type == Type.ACC:
@@ -81,7 +116,7 @@ def play_instructions(instructions: List[Instruction]) -> int:
         elif instruction_type == Type.NOP:
             cursor += 1
 
-    return accumulator
+    return accumulator, cursor >= len(instructions)
 
 
 def _parse(raw_instructions: Iterable[str]) -> List[Instruction]:
@@ -91,6 +126,27 @@ def _parse(raw_instructions: Iterable[str]) -> List[Instruction]:
             raw_instructions
         )
     )
+
+
+def fix_program(instructions: List[Instruction]) -> int:
+    for cursor in range(len(instructions)):
+        new_instruction = _change_instruction(instructions[cursor])
+        if new_instruction:
+            new_instructions = list(instructions)
+            new_instructions[cursor] = new_instruction
+            accumulator, terminate = play_instructions(new_instructions)
+            if terminate:
+                return accumulator
+
+    raise ValueError("Unable to fix the program")
+
+
+def _change_instruction(instruction: Instruction) -> Optional[Instruction]:
+    if instruction.type == Type.NOP or instruction.type == Type.JMP:
+        return Instruction(
+            type=Type.JMP if instruction.type == Type.NOP else Type.NOP,
+            value=instruction.value
+        )
 
 
 def _parse_instruction(raw_instruction: str) -> Instruction:
@@ -108,5 +164,8 @@ if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input")) as file:
         raw_instructions_from_file = list(file.readlines())
 
-        solution_part1 = play_instructions(_parse(raw_instructions_from_file))
+        solution_part1, _ = play_instructions(_parse(raw_instructions_from_file))
         print(f"solution (part1): {solution_part1}")
+
+        solution_part2 = fix_program(_parse(raw_instructions_from_file))
+        print(f"solution (part2): {solution_part2}")
--- tests/day8/test_handheld_halting.py
+++ tests/day8/test_handheld_halting.py
@@ -1,6 +1,6 @@
 from hamcrest import assert_that, equal_to
 
-from aoc.day8.handheld_halting import play_instructions, _parse
+from aoc.day8.handheld_halting import play_instructions, _parse, fix_program
 
 
 class TestPlayInstructions:
@@ -17,9 +17,31 @@ jmp -4
 acc +6"""
 
         # WHEN
-        res = play_instructions(
+        res, _ = play_instructions(
             _parse(instructions.splitlines(keepends=True))
         )
 
         # THEN
         assert_that(res, equal_to(5))
+
+
+class TestFixProgram:
+    def test_should_validate_given_example(self):
+        # GIVEN
+        instructions = """nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"""
+
+        # WHEN
+        res = fix_program(
+            _parse(instructions.splitlines(keepends=True))
+        )
+
+        # THEN
+        assert_that(res, equal_to(8))
