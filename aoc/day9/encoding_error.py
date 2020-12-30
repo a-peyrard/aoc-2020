61a63f4966fd3b1bb83098764f7f0761c3825944
Augustin Peyrard <augustin.peyrard@gmail.com>
2020-12-29 21:33:51 -0800

    adds day9 part2
    implement the first thing I had in my head, a moving buffer...
    I'm wondering if there are better solution for those kind of
    problems 🤔

--- aoc/day9/encoding_error.py
+++ aoc/day9/encoding_error.py
@@ -46,10 +46,39 @@ In this example, after the 5-number preamble, almost every number is the sum of 
  number that does not follow this rule is 127.
 The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble)
  which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
+
+-- Part Two ---
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+Again consider the above example:
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course,
+the contiguous set of numbers in your actual list might be much longer.)
+To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example,
+ these are 15 and 47, producing 62.
+What is the encryption weakness in your XMAS-encrypted list of numbers?
 """
 import os
-from itertools import islice
-from typing import Iterable, List
+from collections import deque
+from typing import Iterable, List, Deque
 
 
 class Preamble:
@@ -84,9 +113,54 @@ def analyze_numbers(numbers: List[int], preamble_size: int = 25) -> int:
     raise ValueError("all numbers are satisfying the condition...")
 
 
+def find_contiguous_set(numbers: List[int], target: int) -> List[int]:
+    start_idx = 0
+    end_idx = 0
+    buf: Deque[int] = deque([numbers[0]])
+    buf_sum = numbers[0]
+
+    len_numbers = len(numbers)
+    while buf_sum != target:
+        if buf_sum < target:
+            if end_idx + 1 >= len_numbers:
+                raise ValueError("no contiguous set")
+            # try to expand the buffer
+            end_idx += 1
+            new_val = numbers[end_idx]
+            buf.append(new_val)
+            buf_sum += new_val
+        else:
+            # buf is too big, let's try to shrink it
+            if start_idx == end_idx:
+                if end_idx + 1 >= len_numbers:
+                    raise ValueError("no contiguous set")
+                # we can not shrink the buffer anymore, create a new one
+                end_idx += 1
+                new_val = numbers[end_idx]
+                buf = deque([new_val])
+                buf_sum = new_val
+            else:
+                old_val = numbers[start_idx]
+                start_idx += 1
+                buf.popleft()
+                buf_sum -= old_val
+
+    return list(buf)
+
+
+def generate_solution_part2(contiguous_set: List[int]):
+    sorted_set = list(sorted(contiguous_set))
+    return sorted_set[0] + sorted_set[-1]
+
+
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input")) as file:
         numbers_from_file = list(map(int, file.readlines()))
 
         solution_part1 = analyze_numbers(numbers_from_file)
         print(f"solution (part1): {solution_part1}")
+
+        solution_part2 = generate_solution_part2(
+            contiguous_set=find_contiguous_set(numbers_from_file, solution_part1)
+        )
+        print(f"solution (part2): {solution_part2}")
--- tests/day9/test_encoding_error.py
+++ tests/day9/test_encoding_error.py
@@ -1,6 +1,6 @@
 from hamcrest import equal_to, assert_that
 
-from aoc.day9.encoding_error import analyze_numbers, Preamble
+from aoc.day9.encoding_error import analyze_numbers, Preamble, find_contiguous_set, generate_solution_part2
 
 
 class TestPreamble:
@@ -79,3 +79,75 @@ class TestAnalyzeNumbers:
 
         # THEN
         assert_that(res, equal_to(127))
+
+
+class TestFindContiguousSet:
+    def test_should_validate_given_example(self):
+        # GIVEN
+        raw_numbers = """35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"""
+
+        # WHEN
+        res = find_contiguous_set(
+            numbers=list(map(int, raw_numbers.splitlines(keepends=True))),
+            target=127
+        )
+
+        # THEN
+        assert_that(res, equal_to([15, 25, 47, 40]))
+
+    def test_should_validate_set_at_the_end(self):
+        # GIVEN
+        numbers = [1, 2, 3, 4, 18]
+
+        # WHEN
+        res = find_contiguous_set(
+            numbers=numbers,
+            target=22
+        )
+
+        # THEN
+        assert_that(res, equal_to([4, 18]))
+
+    def test_should_shrink_the_buffer(self):
+        # GIVEN
+        numbers = [35, 2, 3, 4]
+
+        # WHEN
+        res = find_contiguous_set(
+            numbers=numbers,
+            target=5
+        )
+
+        # THEN
+        assert_that(res, equal_to([2, 3]))
+
+
+class TestGenerateSolutionPart2:
+    def test_should_validate_given_example(self):
+        # GIVEN
+        contiguous_set = [15, 25, 47, 40]
+
+        # WHEN
+        res = generate_solution_part2(contiguous_set)
+
+        # THEN
+        assert_that(res, equal_to(62))
