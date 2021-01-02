2d5bd6dfddc8997ee20c05720abcc58eb57390f7
Augustin Peyrard <augustin.peyrard@gmail.com>
2021-01-01 23:22:39 -0800

    adds day13 part2
    Easy algorithm after seeing this hint: https://streamable.com/tojflp

--- aoc/day13/shuttle_search.py
+++ aoc/day13/shuttle_search.py
@@ -48,9 +48,67 @@ The earliest bus you could take is bus ID 59. It doesn't depart until timestamp 
 What is the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait
 for that bus?
 
+--- Part Two ---
+The shuttle company is running a contest: one gold coin for anyone that can find the earliest timestamp such that the
+first bus ID departs at that time and each subsequent listed bus ID departs at that subsequent minute. (The first line
+in your input is no longer relevant.)
+For example, suppose you have the same list of bus IDs as above:
+7,13,x,x,59,x,31,19
+An x in the schedule means there are no constraints on what bus IDs must depart at that time.
+This means you are looking for the earliest timestamp (called t) such that:
+    Bus ID 7 departs at timestamp t.
+    Bus ID 13 departs one minute after timestamp t.
+    There are no requirements or restrictions on departures at two or three minutes after timestamp t.
+    Bus ID 59 departs four minutes after timestamp t.
+    There are no requirements or restrictions on departures at five minutes after timestamp t.
+    Bus ID 31 departs six minutes after timestamp t.
+    Bus ID 19 departs seven minutes after timestamp t.
+The only bus departures that matter are the listed bus IDs at their specific offsets from t. Those bus IDs can depart
+at other times, and other bus IDs can depart at those times. For example, in the list above, because bus ID 19 must
+depart seven minutes after the timestamp at which bus ID 7 departs, bus ID 7 will always also be departing with bus ID
+19 at seven minutes after timestamp t.
+In this example, the earliest timestamp at which this occurs is 1068781:
+time     bus 7   bus 13  bus 59  bus 31  bus 19
+1068773    .       .       .       .       .
+1068774    D       .       .       .       .
+1068775    .       .       .       .       .
+1068776    .       .       .       .       .
+1068777    .       .       .       .       .
+1068778    .       .       .       .       .
+1068779    .       .       .       .       .
+1068780    .       .       .       .       .
+1068781    D       .       .       .       .
+1068782    .       D       .       .       .
+1068783    .       .       .       .       .
+1068784    .       .       .       .       .
+1068785    .       .       D       .       .
+1068786    .       .       .       .       .
+1068787    .       .       .       D       .
+1068788    D       .       .       .       D
+1068789    .       .       .       .       .
+1068790    .       .       .       .       .
+1068791    .       .       .       .       .
+1068792    .       .       .       .       .
+1068793    .       .       .       .       .
+1068794    .       .       .       .       .
+1068795    D       D       .       .       .
+1068796    .       .       .       .       .
+1068797    .       .       .       .       .
+In the above example, bus ID 7 departs at timestamp 1068788 (seven minutes after t). This is fine; the only requirement
+on that minute is that bus ID 19 departs then, and it does.
+Here are some other examples:
+    The earliest timestamp that matches the list 17,x,13,19 is 3417.
+    67,7,59,61 first occurs at timestamp 754018.
+    67,x,7,59,61 first occurs at timestamp 779210.
+    67,7,x,59,61 first occurs at timestamp 1261476.
+    1789,37,47,1889 first occurs at timestamp 1202161486.
+However, with so many bus IDs in your list, surely the actual earliest timestamp will be larger than 100000000000000!
+What is the earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the
+list?
+
 """
 import os
-from typing import NamedTuple, List, Iterable, Tuple
+from typing import NamedTuple, List, Iterable, Tuple, Optional
 
 
 class ShuttleDeparture(NamedTuple):
@@ -66,6 +124,21 @@ def get_best_shuttle(timestamp: int, shuttle_ids: List[int]) -> ShuttleDeparture
         ),
         key=lambda shuttle: shuttle.waiting_time
     )
+
+
+def get_earliest_timestamp(shuttle_ids: List[int]) -> int:
+    offset = 0
+    step = 1
+    timestamp = 1
+    while len(shuttle_ids) > 0:
+        shuttle_id = shuttle_ids.pop(0)
+        if shuttle_id:
+            while (timestamp + offset) % shuttle_id != 0:
+                timestamp += step
+            step *= shuttle_id
+        offset += 1
+
+    return timestamp
 
 
 def _get_shuttle_info(timestamp: int, shuttle_id: int) -> ShuttleDeparture:
@@ -91,11 +164,23 @@ def _parse(lines: Iterable[str]) -> Tuple[int, List[int]]:
     return timestamp, shuttle_ids
 
 
+def _parse_shuttle_ids(line: str) -> List[Optional[int]]:
+    return [
+        int(token) if token != "x" else None
+        for token in line.rstrip().split(",")
+    ]
+
+
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input")) as file:
-        _timestamp, _shuttle_ids = _parse(file.readlines())
+        raw_lines = list(file.readlines())
+        _timestamp, _shuttle_ids = _parse(raw_lines)
 
         shuttle_part1 = get_best_shuttle(_timestamp, _shuttle_ids)
         solution_part1 = shuttle_part1.id * shuttle_part1.waiting_time
         print(f"solution (part1): {solution_part1}")
         assert solution_part1 == 6559
+
+        solution_part2 = get_earliest_timestamp(_parse_shuttle_ids(raw_lines[1]))
+        print(f"solution (part2): {solution_part2}")
+        assert solution_part2 == 626670513163231
--- tests/day13/test_shuttle_search.py
+++ tests/day13/test_shuttle_search.py
@@ -1,6 +1,6 @@
 from hamcrest import equal_to, assert_that
 
-from aoc.day13.shuttle_search import _parse, get_best_shuttle
+from aoc.day13.shuttle_search import _parse, get_best_shuttle, _parse_shuttle_ids, get_earliest_timestamp
 
 
 class TestGetBestShuttle:
@@ -16,3 +16,89 @@ class TestGetBestShuttle:
 
         # THEN
         assert_that(shuttle.id * shuttle.waiting_time, equal_to(295))
+
+
+class TestGetEarliestTimestamp:
+    def test_should_validate_first_example(self):
+        # GIVEN
+        raw = "7,13,x,x,59,x,31,19"
+
+        # WHEN
+        timestamp = get_earliest_timestamp(
+            _parse_shuttle_ids(raw)
+        )
+
+        # THEN
+        assert_that(timestamp, equal_to(1068781))
+
+    def test_should_validate_second_example(self):
+        # GIVEN
+        raw = "17,x,13,19"
+
+        # WHEN
+        timestamp = get_earliest_timestamp(
+            _parse_shuttle_ids(raw)
+        )
+
+        # THEN
+        assert_that(timestamp, equal_to(3417))
+
+    def test_should_validate_third_example(self):
+        # GIVEN
+        raw = "67,7,59,61"
+
+        # WHEN
+        timestamp = get_earliest_timestamp(
+            _parse_shuttle_ids(raw)
+        )
+
+        # THEN
+        assert_that(timestamp, equal_to(754018))
+
+    def test_should_validate_fourth_example(self):
+        # GIVEN
+        raw = "67,x,7,59,61"
+
+        # WHEN
+        timestamp = get_earliest_timestamp(
+            _parse_shuttle_ids(raw)
+        )
+
+        # THEN
+        assert_that(timestamp, equal_to(779210))
+
+    def test_should_validate_fifth_example(self):
+        # GIVEN
+        raw = "67,7,x,59,61"
+
+        # WHEN
+        timestamp = get_earliest_timestamp(
+            _parse_shuttle_ids(raw)
+        )
+
+        # THEN
+        assert_that(timestamp, equal_to(1261476))
+
+    def test_should_validate_sixth_example(self):
+        # GIVEN
+        raw = "1789,37,47,1889"
+
+        # WHEN
+        timestamp = get_earliest_timestamp(
+            _parse_shuttle_ids(raw)
+        )
+
+        # THEN
+        assert_that(timestamp, equal_to(1202161486))
+
+    def test_should_validate_basic_example(self):
+        # GIVEN
+        raw = "2,3,5"
+
+        # WHEN
+        timestamp = get_earliest_timestamp(
+            _parse_shuttle_ids(raw)
+        )
+
+        # THEN
+        assert_that(timestamp, equal_to(8))
