7b91c56ba77eaa94e17d63f5942889908daa071e
Augustin Peyrard <augustin.peyrard@gmail.com>
2021-01-28 00:16:13 -0800

    change the datastructure to be an array of successors
    So this is not really improving the performances as expected. They
    seems to be close to what we were having before.
    So I guess the main reason was not the datastructure, maybe the algo
    might be optimized...

--- tests/day23/test_crab_cups.py
+++ tests/day23/test_crab_cups.py
@@ -1,4 +1,4 @@
-from hamcrest import equal_to, assert_that, contains_exactly
+from hamcrest import equal_to, assert_that
 
 from aoc.day23.crab_cups import Cups, generate_labels, play_game, generate_prod_part2
 from aoc.util.list import last
@@ -10,31 +10,32 @@ class TestCupsParse:
         raw_cups = "1234"
 
         # WHEN
-        cup = Cups.parse(raw_cups).current_cup
+        cups = Cups.parse(raw_cups)
 
         # THEN
-        assert_that(cup.value, equal_to(1))
-        cup = cup.next
-        assert_that(cup.value, equal_to(2))
-        cup = cup.next
-        assert_that(cup.value, equal_to(3))
-        cup = cup.next
-        assert_that(cup.value, equal_to(4))
+        cup = cups.current
+        assert_that(cup, equal_to(1))
+        cup = cups.get_next(from_cup=cup)
+        assert_that(cup, equal_to(2))
+        cup = cups.get_next(from_cup=cup)
+        assert_that(cup, equal_to(3))
+        cup = cups.get_next(from_cup=cup)
+        assert_that(cup, equal_to(4))
 
     def test_should_parse_cups_and_create_loop(self):
         # GIVEN
         raw_cups = "12"
 
         # WHEN
-        original_cup = Cups.parse(raw_cups).current_cup
+        cups = Cups.parse(raw_cups)
 
         # THEN
-        assert_that(original_cup.value, equal_to(1))
-        cup = original_cup.next
-        assert_that(cup.value, equal_to(2))
-        cup = cup.next
-        assert_that(cup.value, equal_to(1))
-        assert_that(cup == original_cup, equal_to(True))
+        cup = cups.current
+        assert_that(cup, equal_to(1))
+        cup = cups.get_next(from_cup=cup)
+        assert_that(cup, equal_to(2))
+        cup = cups.get_next(from_cup=cup)
+        assert_that(cup, equal_to(1))
 
     def test_should_parse_cups_and_add_additional_cups(self):
         # GIVEN
@@ -60,37 +61,6 @@ class TestCupsRepr:
 
         # THEN
         assert_that(cups.__repr__(), equal_to("1, 2, 3, 4"))
-
-
-class TestCupsInit:
-    def test_should_init_cups(self):
-        # GIVEN
-        raw_cups = "3418"
-
-        # WHEN
-        cups = Cups.parse(raw_cups)
-        cup_3 = cups.current_cup
-        cup_4 = cup_3.next
-        cup_1 = cup_4.next
-        cup_8 = cup_1.next
-
-        # THEN
-        # noinspection PyTypeChecker
-        assert_that(
-            cups._ordered_cups,
-            contains_exactly(
-                cup_8, cup_4, cup_3, cup_1
-            )
-        )
-        assert_that(
-            cups._lookup,
-            equal_to({
-                8: 0,
-                4: 1,
-                3: 2,
-                1: 3
-            })
-        )
 
 
 class TestGenerateLabels:
@@ -157,10 +127,10 @@ class TestCupsMoveToNextCup:
         cups = Cups.parse(raw_cups)
 
         # WHEN
-        current = cups.move_to_next_cup().current_cup
+        current = cups.move_to_next_cup().current
 
         # THEN
-        assert_that(current.value, equal_to(8))
+        assert_that(current, equal_to(8))
 
 
 class TestCupsCut:
@@ -168,9 +138,9 @@ class TestCupsCut:
         # GIVEN
         raw_cups = "389125467"
         cups = Cups.parse(raw_cups)
-        next_cup1 = cups.current_cup.next
-        next_cup2 = next_cup1.next
-        next_cup3 = next_cup2.next
+        next_cup1 = cups.get_next()
+        next_cup2 = cups.get_next(from_cup=next_cup1)
+        next_cup3 = cups.get_next(from_cup=next_cup2)
 
         # WHEN
         cups.cut(to_cup=next_cup3)
@@ -185,24 +155,24 @@ class TestCupsCut:
 class TestCupsInsertAfter:
     def test_should_insert_after_current_cup(self):
         # GIVEN
-        raw_cups = "4987"
+        raw_cups = "4123987"
         cups = Cups.parse(raw_cups)
-
-        cups_to_insert = Cups.parse("123")
-        cup_to_insert = cups_to_insert.current_cup
-        last_cup_to_insert = last(cups_to_insert.iter(), default_value=cup_to_insert)
+        next_cup1 = cups.get_next()
+        next_cup2 = cups.get_next(from_cup=next_cup1)
+        next_cup3 = cups.get_next(from_cup=next_cup2)
+        cups.cut(to_cup=next_cup3)
 
         # WHEN
         cups.insert_after(
-            target_cup=cups.current_cup,
-            start_chain=cup_to_insert,
-            end_chain=last_cup_to_insert
+            target_cup=8,
+            start_chain=next_cup1,
+            end_chain=next_cup3
         )
 
         # THEN
         assert_that(
             cups.__repr__(),
-            equal_to("4, 1, 2, 3, 9, 8, 7")
+            equal_to("4, 9, 8, 1, 2, 3, 7")
         )
 
 
